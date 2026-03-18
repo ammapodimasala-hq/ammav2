42cb4e3a8f1e244e5f6133996ba15acb0cbfa416
ammapodimasala-hq <ammapodimasala@gmail.com>
2026-03-19 01:10:22 +0530

    Update shiprocket.js

--- api/shiprocket.js
+++ api/shiprocket.js
@@ -1,1 +1,17 @@
 
+export default async function handler(req, res) {
+  const response = await fetch("https://apiv2.shiprocket.in/v1/external/auth/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      email: process.env.SHIPROCKET_EMAIL,
+      password: process.env.SHIPROCKET_PASSWORD
+    })
+  });
+
+  const data = await response.json();
+
+  res.status(200).json(data);
+}
