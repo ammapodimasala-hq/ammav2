2d99bf88958e1cd1d5a7c36f9c5fad0bcf7692b3
ammapodimasala-hq <ammapodimasala@gmail.com>
2026-03-20 19:03:34 +0530

    Update create-order.js

--- api/create-order.js
+++ api/create-order.js
@@ -13,8 +13,8 @@ export default async function handler(req, res) {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        email: process.env.pratiknj2901@gmail,
-        password: process.env.Pratik@sr1
+        email: process.env.SR_EMAIL,
+        password: process.env.SR_PASSWORD
       })
     });
 
