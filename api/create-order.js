2aa4724e5d3f189a7cb68a7bd7434f3d1848b57e
ammapodimasala-hq <ammapodimasala@gmail.com>
2026-03-26 02:35:34 +0530

    Update create-order.js

--- api/create-order.js
+++ api/create-order.js
@@ -1,15 +1,22 @@
 const Razorpay = require("razorpay");
 
 module.exports = async (req, res) => {
-  const instance = new Razorpay({
-    key_id: "rzp_test_xxx", // paste directly //
-    key_secret: "xxxxxx"
-  });
+  try {
+    const instance = new Razorpay({
+      key_id: "rzp_test_SVb8lg207s9Fqr",       // paste fresh test key
+      key_secret: "GPtBYjT8osQVlIVg06k7YstT"         // paste matching secret
+    });
 
-  const order = await instance.orders.create({
-    amount: 50000,
-    currency: "INR"
-  });
+    const order = await instance.orders.create({
+      amount: 50000,
+      currency: "INR",
+      receipt: "receipt_order_1"
+    });
 
-  res.status(200).json(order);
+    return res.status(200).json(order);
+
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: err.message });
+  }
 };
