20b1db55d7a0ad0a938a93a299259170d83e40e2
ammapodimasala-hq <ammapodimasala@gmail.com>
2026-03-26 02:15:12 +0530

    Update create-order.js

--- api/create-order.js
+++ api/create-order.js
@@ -1,18 +1,8 @@
 const Razorpay = require("razorpay");
 
 module.exports = async (req, res) => {
-  try {
-    console.log("ENV CHECK:");
-    console.log("KEY_ID:", process.env.RAZORPAY_KEY_ID);
-    console.log("KEY_SECRET:", process.env.RAZORPAY_KEY_SECRET);
-
-    return res.status(200).json({
-      key_id: process.env.RAZORPAY_KEY_ID,
-      key_secret: process.env.RAZORPAY_KEY_SECRET ? "EXISTS" : "MISSING"
-    });
-
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: err.message });
-  }
-};
+  return res.status(200).json({
+    KEY_ID: process.env.RAZORPAY_KEY_ID || "MISSING",
+    KEY_SECRET: process.env.RAZORPAY_KEY_SECRET ? "EXISTS" : "MISSING"
+  });
+};;
